ec685a3ae13d38f6c0b4dbbc6aaaf86e447c6946
AMansfield <ad_mansfield@yahoo.com>
2010-11-30 17:47:59 -0800

    Added the ability to select and queue multiple files.  Also made indentations more consistent.

--- Desktop/flingo.py
+++ Desktop/flingo.py
@@ -15,105 +15,106 @@ from PyQt4 import QtCore, QtGui
 from ConfigParser import RawConfigParser
 
 try:
-    from win32com.shell import shellcon, shell            
-    HOMEDIR = shell.SHGetFolderPath(0, shellcon.CSIDL_MYVIDEO, 0, 0)
- 
+   from win32com.shell import shellcon, shell
+   HOMEDIR = shell.SHGetFolderPath(0, shellcon.CSIDL_MYVIDEO, 0, 0)
+
 except ImportError: # quick semi-nasty fallback for non-windows/win32com case
-    HOMEDIR = os.path.expanduser("~")
+   HOMEDIR = os.path.expanduser("~")
 
 def update_config(conf_file, config={}):
-    c = config
-    if conf_file and os.path.isfile(conf_file):
-        rv = RawConfigParser()
-        rv.read(conf_file)
-        if rv.has_section('fling'):
-            for k,v in rv.items('fling'):
-                c[k] = v
-    return c
-    
+   c = config
+   if conf_file and os.path.isfile(conf_file):
+      rv = RawConfigParser()
+      rv.read(conf_file)
+      if rv.has_section('fling'):
+         for k,v in rv.items('fling'):
+            c[k] = v
+   return c
+
 def find_config():
-    config = update_config('flingo.conf')
-    conf = os.getenv('FLING_CONF', default='')
-    if conf:
-        config = update_config(conf, config)
-    return config
+   config = update_config('flingo.conf')
+   conf = os.getenv('FLING_CONF', default='')
+   if conf:
+      config = update_config(conf, config)
+   return config
 
 def get_local_ip():
-    addr = socket.gethostbyname(socket.gethostname())
-    if addr and addr!='127.0.0.1':
-    	return addr
-    return None
+   addr = socket.gethostbyname(socket.gethostname())
+   if addr and addr!='127.0.0.1':
+      return addr
+   return None
 
-config = find_config()    
+config = find_config()
 PORT = int(config.get('port', 8080))
 FLING_ADDR_BASE = config.get('host', 'http://flingo.tv')
 DEVICE_CHECK = FLING_ADDR_BASE + '/fling/has_devices'
 FLING_URL = FLING_ADDR_BASE + '/fling/fling'
-    
-class FlingIcon(QtGui.QSystemTrayIcon):
-    def __init__(self, parent=None):
-        QtGui.QSystemTrayIcon.__init__(self, parent)
-        
-        self.menu = QtGui.QMenu(parent)
-        self.menu.addAction('Fling File', self.find)
-        #self.menu.addAction('Quit', self.quit)
-        
-        quit=self.menu.addAction("Quit")
-        self.connect(quit,QtCore.SIGNAL('triggered()'),self.quit)
-        
-        self.file = QtGui.QFileDialog(None)
-        self.file.setFileMode(QtGui.QFileDialog.ExistingFile)
-        #self.file.setFilter("Video Files (*.mp4)")
-        self.file.setDirectory(HOMEDIR)
-        self.file.setReadOnly(True)
-        self.setContextMenu(self.menu)
-        self.icon = QtGui.QIcon('flingo.png')
-        self.setIcon(self.icon)
 
-    def quit(self):
-        QtGui.QApplication.quit()
-        sys.exit()
-    
-    def find(self):
-        response = {}
-        try:
-            req = urllib2.Request(DEVICE_CHECK)
-            response = json.loads(urllib2.urlopen(req).read())
-        except Exception,e:
-            print str(e)
-        
-        if response==True:
+class FlingIcon(QtGui.QSystemTrayIcon):
+   def __init__(self, parent=None):
+      QtGui.QSystemTrayIcon.__init__(self, parent)
+
+      self.menu = QtGui.QMenu(parent)
+      self.menu.addAction('Fling File', self.find)
+      #self.menu.addAction('Quit', self.quit)
+
+      quit=self.menu.addAction("Quit")
+      self.connect(quit,QtCore.SIGNAL('triggered()'),self.quit)
+
+      self.file = QtGui.QFileDialog(None)
+      self.file.setFileMode(QtGui.QFileDialog.ExistingFiles)
+      #self.file.setFilter("Video Files (*.mp4)")
+      self.file.setDirectory(HOMEDIR)
+      self.file.setReadOnly(True)
+      self.setContextMenu(self.menu)
+      self.icon = QtGui.QIcon('flingo.png')
+      self.setIcon(self.icon)
+
+   def quit(self):
+      QtGui.QApplication.quit()
+      sys.exit()
+
+   def find(self):
+      response = {}
+      try:
+         req = urllib2.Request(DEVICE_CHECK)
+         response = json.loads(urllib2.urlopen(req).read())
+      except Exception,e:
+         print str(e)
+
+         if response==True:
             self.fling()
-        else:
+         else:
             self.warning()
-            
-    def warning(self):
-        QtGui.QMessageBox.warning(self.menu,"Warning","No flingable devices were found.")
-        
-    def fling(self):
-        try:
-            fileNames = []
-            if (self.file.exec_()):
-                fileNames = self.file.selectedFiles()
+
+   def warning(self):
+      QtGui.QMessageBox.warning(self.menu,"Warning","No flingable devices were found.")
+
+   def fling(self):
+      try:
+         fileNames = []
+         if (self.file.exec_()):
+            fileNames = self.file.selectedFiles()
 
             if fileNames:
-		fileName = str(fileNames[0])
-		if sys.platform=='win32':
-		    fileName = fileName.replace("C:","")
-		name = os.path.basename(fileName)
-                params = {}
-                params['url'] = 'http://' + get_local_ip() +':' + str(PORT) + fileName 
-                params['description'] = 'Desktop Fling of %s from %s' % (name, socket.gethostname())
-                params['title'] = '%s via Desktop Fling' % name
-                data = urllib.urlencode(params)
-		req = urllib2.Request(FLING_URL, data)
-                response = urllib2.urlopen(req).read()
-        except Exception, e:
-            print str(e)
+               for fName in fileNames:
+                  fileName = str(fName)
+                  if sys.platform=='win32':
+                     fileName = fileName.replace("C:","")
+                  name = os.path.basename(fileName)
+                  params = {}
+                  params['url'] = 'http://' + get_local_ip() +':' + str(PORT) + fileName
+                  params['description'] = 'Desktop Fling of %s from %s' % (name, socket.gethostname())
+                  params['title'] = '%s via Desktop Fling' % name
+                  data = urllib.urlencode(params)
+                  req = urllib2.Request(FLING_URL, data)
+                  response = urllib2.urlopen(req).read()
+      except Exception, e:
+         print str(e)
 
 app = QtGui.QApplication([])
 qt4reactor.install()
-       
+
 i = FlingIcon()
 i.show()
 
@@ -123,9 +124,9 @@ from twisted.web.static import File
 
 root = "/"
 if sys.platform == 'win32':
-    root = "c:\\"
+   root = "c:\\"
 doc_root = File(root)
 site = Site(doc_root)
 reactor.listenTCP(PORT, site)
 app.exec_()
-reactor.run() 
+reactor.run()
