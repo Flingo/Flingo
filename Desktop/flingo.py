1fd512b01661ccf7dec0206eb30fc8c43c28d542
dosirrah <dave@flingo.tv>
2011-01-22 14:48:04 -0800

    Add more robust get_local_ips based on netifaces.
    My get_local_ips based on socket.gethostbyname was only finding the loopback
    interface.  This change has the drawback that it requires the developer
    to install netifaces.  However netifac

--- Desktop/flingo.py
+++ Desktop/flingo.py
@@ -5,12 +5,23 @@
 #
 # author: Omar Zennadi
 
-import os
-import sys
-import qt4reactor
-import urllib, urllib2
 import json
+import netifaces
+try:
+    from netifaces import interfaces, ifaddresses
+    found_netifaces = True
+catch ImportError,e:
+    print "WARNING! Failed to import netifaces.  You can obtain netifaces on Windows "
+    print "and OSX by running:"
+    print "  easy_install netifaces"
+    print "Using the less reliable socket.gethostbyname to determine ip address."
+    found_netifaces = False
+
+import os
+import qt4reactor
 import socket
+import sys
+import urllib, urllib2
 from PyQt4 import QtCore, QtGui
 from ConfigParser import RawConfigParser
 
@@ -32,23 +43,37 @@ def update_config(conf_file, config={}):
    return c
 
 def find_config():
-   config = update_config('flingo.conf')
-   conf = os.getenv('FLING_CONF', default='')
-   if conf:
-      config = update_config(conf, config)
-   return config
+    config = update_config('flingo.conf')
+    conf = os.getenv('FLING_CONF', default='')
+    if conf:
+        config = update_config(conf, config)
+    return config
 
-def get_local_ip():
-   addr = socket.gethostbyname(socket.gethostname())
-   if addr and addr!='127.0.0.1':
-      return addr
-   return None
+def get_local_ips():
+   """Returns the set of known local IP addresses excluding the loopback 
+      interface."""
+   global found_netifaces
+   ips = []
+   if found_netifaces:
+       ifs = [ifaddresses(i).get(netifaces.AF_INET,None) for i in interfaces()]
+       ifs = [i for i in ifs if i]
+       ips = []
+       for i in ifs:
+         ips.extend([j.get('addr',None) for j in i])
+       ips = [i for i in ips if i and i != "127.0.0.1"]
+   if not found_netifaces or not ips:
+       ip = socket.gethostbyname(socket.gethostname())
+       if ip != "127.0.0.1":
+         ips.append(ip)
+   return ips
 
 config = find_config()
 PORT = int(config.get('port', 8080))
 FLING_ADDR_BASE = config.get('host', 'http://flingo.tv')
-DEVICE_CHECK = FLING_ADDR_BASE + '/fling/has_devices'
+#FLING_ADDR_BASE = "http://dave.flingo.tv"
+DEVICE_CHECK = FLING_ADDR_BASE + '/fling/has_services'
 FLING_URL = FLING_ADDR_BASE + '/fling/fling'
+print "DEVICE_CHECK=", DEVICE_CHECK
 
 class FlingIcon(QtGui.QSystemTrayIcon):
    def __init__(self, parent=None):
@@ -77,6 +102,7 @@ class FlingIcon(QtGui.QSystemTrayIcon):
    def find(self):
       response = {}
       try:
+         print "searching for devices..."
          req = urllib2.Request(DEVICE_CHECK)
          response = json.loads(urllib2.urlopen(req).read())
       except Exception,e:
@@ -96,25 +122,30 @@ class FlingIcon(QtGui.QSystemTrayIcon):
          if (self.file.exec_()):
             fileNames = self.file.selectedFiles()
 
-            if fileNames:
-               for fName in fileNames:
-                  fileName = str(fName)
-                  if sys.platform=='win32':
-                     fileName = fileName.replace("C:","")
-                  name = os.path.basename(fileName)
-                  params = {}
-                  params['url'] = 'http://' + get_local_ip() +':' + str(PORT) + fileName
-                  params['description'] = 'Desktop Fling of %s from %s' % (name, socket.gethostname())
-                  params['title'] = '%s via Desktop Fling' % name
-                  data = urllib.urlencode(params)
-                  req = urllib2.Request(FLING_URL, data)
-                  response = urllib2.urlopen(req).read()
+            for fName in fileNames:
+               fileName = str(fName)
+               if sys.platform=='win32':
+                  fileName = fileName.replace("C:","")
+               name = os.path.basename(fileName)
+               params = {}
+               ips = get_local_ips()
+               if not ips:
+                   print "Could not find ip address."
+                   return
+
+               params['url'] = 'http://' + ips[0] +':' + str(PORT) + fileName
+               params['description'] = 'Desktop Fling of %s from %s' % (name, socket.gethostname())
+               params['title'] = '%s via Desktop Fling' % name
+               data = urllib.urlencode(params)
+               req = urllib2.Request(FLING_URL, data)
+               response = urllib2.urlopen(req).read()
       except Exception, e:
          print str(e)
 
 app = QtGui.QApplication([])
 qt4reactor.install()
 
+print "Creating FlingIcon"
 i = FlingIcon()
 i.show()
 
@@ -126,6 +157,8 @@ root = "/"
 if sys.platform == 'win32':
    root = "c:\\"
 doc_root = File(root)
+
+print "Starting reactor"
 site = Site(doc_root)
 reactor.listenTCP(PORT, site)
 app.exec_()
