1c867e0d4d244cbe232c6878de54db7c6350db16
dosirrah <dave@flingo.tv>
2012-04-28 02:21:49 -0700

    Command-line version now uses twisted in order to improve range requests.
    I don't particularly like twisted.  I think gevent is a much better
    server, but twisted is already a dependency to build the desktop version,
    and I don't feel like doing much work right now to use gevent.
    
    Also added support for the "--back" command-line argument which allows
    the caller to push to the back of the queue.  Without this argument,
    flingo.py pushes to the front of the user's queue.

--- CommandLine/flingo.py
+++ CommandLine/flingo.py
@@ -10,8 +10,17 @@ import httplib
 import socket
 from urlparse import urlparse
 from urllib import quote
-import SimpleHTTPServer
+
+# SimpleHTTPServer does not support range requests, which causes fling
+# to not operate on many TVs.  twisted does support range requests.
+#import SimpleHTTPServer
 import SocketServer
+
+# twisted has some external dependencies (Blah) but it supports range
+# requests.
+from twisted.internet import reactor
+from twisted.web.server import Site
+from twisted.web.static import File
 
 # In OS X, the following doesn't work as desired:
 #   dname, temp, ips = socket.gethostbyname_ex(socket.gethostname())
@@ -26,7 +35,6 @@ except ImportError,e:
     print "  easy_install netifaces"
     print "Using the less reliable socket.gethostbyname to determine ip address."
     found_netifaces = False
-
 
 
 
@@ -50,7 +58,7 @@ def get_local_ips():
    if not found_netifaces or not ips:
        ip = socket.gethostbyname(socket.gethostname())
        if ip != "127.0.0.1":
-         ips.append(ip)
+           ips.append(ip)
    return ips
 
 
@@ -59,125 +67,116 @@ def get_local_ips():
 # where the file came from.  Even if a description is provided,
 # where the file came from is appended to the description.
 #
-def fling( path, port=PORT, title=None, description=None ):
-  if type(port) == str:
-    port = int(port)
-  print "fling %s from port %d with title %s" % (path, port, title)
-  if os.path.isabs(path):
-    print "Use relative paths.  %s is absolute" % path
-    reactor.stop()
-
-  p, ext = os.path.splitext( path )
-  d, fname = os.path.split(p)
-  if not d:
-    d = os.getcwd()
-
-  dname = socket.gethostname()
-  ips = get_local_ips()
-  ip = ips[0]  # HEREDAVE: need to be smarter.
-
-  if not title:
-    title = fname
-
-  if dname and dname != "localhost":
-    flung_across_lan = "Flung across local network from machine %s" % (
-      "with name %s at ip address %s." % ( dname, ip ))
-  else:
-    flung_across_lan = "Flung across local network from machine %s " % (
-      "with ip address %s." % ip)
-
-  if not description: 
-    description = ""
-  description += flung_across_lan
-
-  url = "http://%s:%d/%s" % ( ip, port, path )
-
-  print "title:",title
-  print "description:", description
-  print "url:", url
-
-  fling_url = "http://flingo.tv/fling/fling?%s" % (  
-    "title=%s&description=%s&url=%s&version=%s"%(
-    quote(title), quote(description), quote(url), "1.0.12" ))
-
-  print "fling_url:", fling_url
-  result = get(fling_url)
-
-  print "result:", result
+def fling( path, port=PORT, title=None, description=None, front=True ):
+    if type(port) == str:
+        port = int(port)
+    print "fling %s from port %d with title %s" % (path, port, title)
+    if os.path.isabs(path):
+        print "Use relative paths.  %s is absolute" % path
+        reactor.stop()
+  
+    p, ext = os.path.splitext( path )
+    d, fname = os.path.split(p)
+    if not d:
+        d = os.getcwd()
+  
+    dname = socket.gethostname()
+    ips = get_local_ips()
+    ip = ips[0]  # HEREDAVE: need to be smarter.
+  
+    if not title:
+        title = fname
+  
+    if dname and dname != "localhost":
+        flung_across_lan = "Flung across local network from machine %s" % (
+            "with name %s at ip address %s." % ( dname, ip ))
+    else:
+        flung_across_lan = "Flung across local network from machine %s " % (
+            "with ip address %s." % ip)
+  
+    if not description: 
+        description = ""
+    description += flung_across_lan
+  
+    url = "http://%s:%d/%s" % ( ip, port, path )
+  
+    print "title:",title
+    print "description:", description
+    print "url:", url
+  
+    fling_url = "http://flingo.tv/fling/fling?%s" % (  
+        "title=%s&description=%s&url=%s&version=%s&front=%s"%(
+        quote(title), quote(description), quote(url), "1.0.12", front ))
+  
+    print "fling_url:", fling_url
+    result = get(fling_url)
+  
+    print "result:", result
 
 
 # pulls down resource from given URL and returns the response body as a string.
 def get(url):
-  (scheme, netloc, path, pars, query, fragment) = urlparse(url)
-  lst = netloc.split(":")
-  if len(lst) == 2:
-    host, port = lst
-  else:
-    host, port = lst[0], 80
-
-  conn = httplib.HTTPConnection(netloc, port)
-
-  # http://foo.com/a/b/c?do=blah&po=fa --> /foo.com/a/b/c?do=blah&po=fa
-  long_path = "/" + "/".join(url.split("/")[3:])
-  conn.request("GET", long_path )
-
-  r = conn.getresponse()
-  if r.status != 200:
-    raise Exception( "** fling encountered error. returned status: %s %s" % (
-      r.status, r.reason ))
-  data = r.read()
-  return data
+    (scheme, netloc, path, pars, query, fragment) = urlparse(url)
+    lst = netloc.split(":")
+    if len(lst) == 2:
+        host, port = lst
+    else:
+        host, port = lst[0], 80
+    
+    conn = httplib.HTTPConnection(netloc, port)
+    
+    # http://foo.com/a/b/c?do=blah&po=fa --> /foo.com/a/b/c?do=blah&po=fa
+    long_path = "/" + "/".join(url.split("/")[3:])
+    conn.request("GET", long_path )
+    
+    r = conn.getresponse()
+    if r.status != 200:
+        raise Exception("** fling encountered error. returned status: %s %s"%(
+            r.status, r.reason ))
+    data = r.read()
+    return data
 
 
 if __name__ == "__main__":
-  from optparse import OptionParser
-  try:
+    from optparse import OptionParser
+    try:
 
-    usage = """usage: %prog [options] FILE\nThe file is sent (i.e., 
+        usage = """usage: %prog [options] FILE\nThe file is sent (i.e., 
 private broadcast to all fling-enabled devices (TVs, bluray) in your local
 network.  To find the flung item, start your device and go to the fling-enabled
 app (called "Web Videos" on Vizio VIA TVs).  The item should appear at the front of your queue.
-"""
-
-    parser = OptionParser(usage=usage)
-    #parser.add_option("-i", "--image", dest="image",
-    #  help="box art used to generate thumbs and larger images.  If no image " +
-    #       "is provided then fling.py searches the current directory.  If " +
-    #       "the directory has only one image then it is used.  If there is " +
-    #       "more than one image then it searches for one with a name "
-    #       "that matches flung file's name.", default="")
-    parser.add_option("-d", "--description", dest="description",
-                      help="description of the file to be flung.", default="" )
-    parser.add_option("-t", "--title", dest="title",
-                      help="title of the file to be flung.", default="")
-    parser.add_option("-p", "--port", dest="port",
-                      help="port number from which to serve flung files.",
-                      default=PORT)
+    """
     
-    (options, args) = parser.parse_args()
-
-    if len(args) > 1:
-      parse.print_usage()
-      sys.exit(-1)
-
-    fname = args[0]
-    if type(options.port) in [str,unicode]:
-      options.port = int(options.port)
-    fling( fname, options.port, options.title, options.description )
-
-    root_dir = os.getcwd()
-
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        parser = OptionParser(usage=usage)
+        parser.add_option("-d", "--description", dest="description",
+                          help="description of the file to be flung.", 
+                          default="" )
+        parser.add_option("-t", "--title", dest="title",
+                          help="title of the file to be flung.", default="")
+        parser.add_option("-p", "--port", dest="port",
+                          help="port number from which to serve flung files.",
+                          default=PORT)
+        parser.add_option("-b", "--back", dest="front", action="store_false",
+                          help="push to the back of the queue.", default=True )
+        
+        (options, args) = parser.parse_args()
     
-    httpd = SocketServer.TCPServer(("", options.port), Handler)
+        if len(args) > 1:
+            parse.print_usage()
+            sys.exit(-1)
     
-    print "serving at port", options.port
-    httpd.serve_forever()
-
-    #server = HTTPServer(('',options.port), FlingHandler )
-    #print 'started Fling server on %d' % options.port
-    #server.serve_forever()
-
-  except KeyboardInterrupt:
-    print '\nCTRL-C received, shutting down'
-    server.socket.close()
+        fname = args[0]
+        if type(options.port) in [str,unicode]:
+            options.port = int(options.port)
+        fling( fname, options.port, options.title, options.description,
+            front = options.front )
+    
+        root_dir = os.getcwd()
+    
+        doc_root = File(root_dir)
+        site = Site(doc_root)
+        reactor.listenTCP(PORT, site)
+        reactor.run()
+    
+    except KeyboardInterrupt:
+        print '\nCTRL-C received, shutting down'
